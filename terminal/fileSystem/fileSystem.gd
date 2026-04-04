010d668f0579155066afe6e80c8da425e1715ee6
João Ítalo <161601649+joaoitaloal@users.noreply.github.com>
2026-04-04 18:09:23 -0300

    Inicio do sistema de arquivos

---
+++ terminal/fileSystem/fileSystem.gd
@@ -0,0 +1,99 @@
+class_name FileSystem
+extends Node
+
+# Entidade que é responsável por guardar 
+# e navegar pelo sistema de arquivos
+
+# Opções pro sistema de arquivos(Escolhi a primeira opção):
+# Uma árvore em que cada pasta aponta pras pastas dentro dela
+# Um dicionario (com que estrutura não sei), se a gente se importar com o tempo de acesso
+
+var root: Folder = Folder.new("", null);
+
+# Pasta sendo navegada atualmente
+var cur_folder: Folder = root;
+var cur_path: String = "/"; # Caminho atual, pro display no terminal
+
+func navigate(path: String):
+	var folder = resolve_path(path, true);
+	if(folder):
+		cur_folder = folder;
+		cur_path = get_full_path(folder);
+
+# TODO o mkdir avisa se o folder foi criado com sucesso ou não?
+func create_folder(path: String):
+	var formatted = format_path(path);
+	var folder_name = formatted.tokens[formatted.tokens.size()-1]
+	formatted.tokens.remove_at(formatted.tokens.size()-1);
+	
+	# folder pai do folder que vai ser criado
+	var folder = resolve_path_tokens(formatted.tokens, formatted.cur);
+	if(folder): 
+		folder.create_folder(folder_name);
+		return true;
+	return false;
+
+# TODO não sei se o touch avisa tbm
+func create_file(path: String):
+	var formatted = format_path(path);
+	var file_name = formatted.tokens[formatted.tokens.size()-1]
+	formatted.tokens.remove_at(formatted.tokens.size()-1);
+	
+	# folder pai do arquivo que vai ser criado
+	var folder = resolve_path_tokens(formatted.tokens, formatted.cur);
+	if(folder): 
+		folder.create_file(file_name);
+		return true;
+	return false;
+
+func list_files(path: String) -> Array[File]:
+	return resolve_path(path).get_files();
+
+func list_folders(path: String) -> Array[Folder]:
+	var folder = resolve_path(path);
+	return folder.get_folders();
+
+# Função pra pegar o caminho de uma pasta a partir do root
+func get_full_path(folder: Folder) -> String:
+	var path = folder.folder_name;
+	var cur = folder.get_folder("..");
+	while cur:
+		path = cur.folder_name + "/" + path;
+		cur = cur.get_folder("..");
+	return "/" if path == "" else path;
+	
+func format_path(path: String):
+	path = path.strip_edges();
+	
+	var absolute = false;
+	
+	if(path.begins_with("./")): path = path.replace("./", "");
+	elif(path.begins_with("/")): absolute = true;
+	
+	var tokens = path.split("/", false);
+	var cur: Folder = root if absolute else cur_folder;
+	
+	return {
+		"tokens": tokens,
+		"cur": cur
+	}
+
+# Isso tá separado em duas funções pq as vezes
+# vai ser util modificar os tokens antes de resolver o path,
+# a função de cima é uma função de conveniência basicamente
+func resolve_path(path: String, absolute: bool = false) -> Folder:
+	var formated = format_path(path);
+	var tokens = formated.tokens;
+	var cur = formated.cur;
+	
+	return resolve_path_tokens(tokens, cur, absolute);
+func resolve_path_tokens(tokens: PackedStringArray, cur: Folder, absolute: bool = false):
+	if(tokens.size() == 0): return root if absolute else cur;
+	
+	# Ignorar caso o usuário coloque um "/" no fim do caminho
+	if(tokens[tokens.size()-1] == ""): tokens.remove_at(tokens.size()-1);
+	
+	for token in tokens:
+		cur = cur.get_folder(token);
+		if(cur == null): break;
+	return cur;
--- terminal/terminal.gd
+++ terminal/terminal.gd
@@ -1,25 +1,56 @@
 class_name Terminal
 extends Control
 
+# TODO checar o comportamento de todos esses comandos pra ver se tão certos
+# TODO acho que tá faltando vários feedbacks qnd o comando não é completado com sucesso
+
 var ACTIONS = {
 	"clear": clear,
-	"echo": echo
+	"echo": echo,
+	"cd" : cd,
+	"ls": ls,
+	"mkdir": mkdir,
+	"touch": touch
 }
 
-func _process(_delta):
-	if(%Input.has_focus() && Input.is_action_just_pressed("ui_text_submit")):
-		_on_submit_pressed();
+func _ready():
+	%DisplayPath.text = %FileSystem.cur_path;
 
-# Função de uso interno
-func t_print(text: String):
-	%Output.add_text(text);
+# Função de uso interno, o texto tem que ser sanitizado por causa do BBcode
+func t_print(text_: String):
+	%Output.append_text(text_);
 	%Output.newline();
 
 func clear(_args: PackedStringArray):
 	%Output.clear();
 
 func echo(args: PackedStringArray):
-	t_print(" ".join(args));
+	var text = " ".join(args);
+	text = text.replace("[", "[lb]")
+	t_print(text);
+
+func cd(args: PackedStringArray):
+	%FileSystem.navigate("".join(args));
+	%DisplayPath.text = %FileSystem.cur_path;
+
+func ls(args: PackedStringArray):
+	var path = "".join(args);
+	var folders = %FileSystem.list_folders(path);
+	var files = %FileSystem.list_files(path);
+	
+	var tokens = PackedStringArray();
+	for folder: Folder in folders:
+		tokens.push_back("[color=#A0A0FF]%s[/color]" % folder.folder_name);
+	for file: File in files:
+		tokens.push_back("[color=#FFA0A0]%s[/color]" % file.file_name);
+	tokens.sort();
+	t_print(" ".join(tokens));
+
+func mkdir(args: PackedStringArray):
+	%FileSystem.create_folder("".join(args));
+
+func touch(args: PackedStringArray):
+	%FileSystem.create_file("".join(args));
 
 func _on_submit_pressed():
 	var args: PackedStringArray =  %Input.text.split(" ");
@@ -29,6 +60,10 @@ func _on_submit_pressed():
 	if(ACTIONS.get(cmd)):
 		ACTIONS[cmd].call(args);
 	else:
-		t_print("O comando %s não existe" % cmd);
+		echo(PackedStringArray(["O comando %s não existe" % cmd]));
 	
 	%Input.text = "";
+
+
+func _on_input_text_submitted(_new_text):
+	_on_submit_pressed();
