2ddf750c4f4513c18db7aea2139586658abc5ec4
Eric <eric.rodrig.gomes@gmail.com>
2026-04-09 22:29:40 -0300

    Mudanças na organização e no menu principal

--- Menus/Menu principal/menu_principal.gd
+++ Menus/Menu principal/menu_principal.gd
@@ -4,5 +4,10 @@ extends Control
 func _ready() -> void:
 	pass # Replace with function body.
 
+#TODO: Colocar para aparecer mensagem de confirmaçao
 func _on_exit_pressed():
 	get_tree().quit();
+
+func _on_config_pressed():
+	# colocar a tela de config
+	get_tree().change_scene_to_file("res://Menu principal/menu_principal.tscn")
--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # ctrl-escape
-
