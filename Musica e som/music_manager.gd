7b3b6593bc1c05d8016694a84a2bef051957330f
Bianca <biacavalcantemorais@gmil.com>
2026-04-23 20:55:28 -0300

    Visual terminal

--- Musica e som/music_manager.gd
+++ Musica e som/music_manager.gd
@@ -8,28 +8,24 @@ func _ready() -> void:
 	bus_index = AudioServer.get_bus_index("Musica")
 	carregar_e_aplicar_volume_inicial()
 	
-	# Configuração do player
-	player.stream = preload("res://Musica e som/851360__josefpres__piano-loops-207-octave-long-loop-120-bpm.wav")
+	player.stream = preload("res://Musica e som/851360__josefpres__piano-loops-207-octave-long-loop-120-bpm_[cut_16sec].mp3")
 	player.bus = "Musica"
 	if not player.playing:
 		player.play()
 
 func carregar_e_aplicar_volume_inicial():
 	var err = config.load("user://settings.cfg")
-	var v = -10 # Valor padrão
+	var v = -10
 	if err == OK:
 		v = config.get_value("audio", "music_volume", -10)
 	
-	# Aplica o volume no servidor de áudio
 	if bus_index != -1:
 		AudioServer.set_bus_volume_db(bus_index, v)
 
 func atualizar_volume(novo_valor: float):
-	# Esta função será chamada pelo Slider do Menu
 	if bus_index != -1:
 		AudioServer.set_bus_volume_db(bus_index, novo_valor)
-	
-	# Salva no arquivo
+
 	config.set_value("audio", "music_volume", novo_valor)
 	config.save("user://settings.cfg")
 
